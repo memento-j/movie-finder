b6bfcdbae9e8ebd7cd3b2d3252b6791f35b0996e
jsales2 <jsales0204@gmail.com>
2025-04-16 16:08:07 -0400

    added functionality to get data from api

--- Movie-Finder/script.js
+++ Movie-Finder/script.js
@@ -1,25 +1,63 @@
-//importing api key
 import { apiKey } from "./ignore/key.js";
 
-//get user query (name of movie to search)
-const movieName = "spiderman";
+const searchInput = document.getElementById("searchBar");
 
+//sets movieName variable so searching can also be done by clicking the magnifying glass image
+let movieName;
+searchInput.addEventListener("keyup", (event) => {
+  //binds input to the movieName variable
+  movieName = event.target.value;
+})
 
-
-
-
-const url = `https://api.themoviedb.org/3/search/movie?query=${movieName}&include_adult=false&language=en-US&page=1`;
-const options = {
-  headers: {
-    accept: 'application/json',
-    Authorization: `Bearer ${apiKey}`
+//get user query (name of movie to search) and searches for the movie
+//once the api call is ran, 
+searchInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    const jsonMovieData = searchMovie(searchInput.value);
+    displayMoiveData(jsonMovieData);
+    
   }
-};
+})
 
-fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log(json))
-  .catch(err => console.error(err));
+//searches movie when search icon is clicked
+const searchImg = document.getElementById("searchImage");
+searchImg.addEventListener('click', () => {
+  const jsonMovieData = searchMovie(movieName);
+  displayMoiveData(jsonMovieData);
+  
+});
+
+
+//searches movie based on name and returns json object with movies
+async function searchMovie(movieName) {
+  //sets url and options for the fetch function call
+  const url = `https://api.themoviedb.org/3/search/movie?query=${movieName}&include_adult=false&language=en-US&page=1`;
+  const options = {
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${apiKey}`
+    }
+  };
+  
+  //runs api call, gets reponse, then converts data to json to be returned
+  try {
+    const response = await fetch(url, options);
+    return await response.json();
+    //catches error if it occurs
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+//display movie data
+function displayMoiveData(jsonMovieData) {
+  console.log(jsonMovieData)
+
+  //can display image of movie, release date/year, overview, genres/movie title/average rating out of 10 with total vote count
+  
+  //format with css
+}
 
 
 
